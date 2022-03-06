0fb089817ec86b6541d88d6357f65ae5542e3077
luckystoned <luckystoned.dev@gmail.com>
2022-03-05 22:16:17 -0300

    add themeprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
+import {
+  useEffect
+} from 'react';
 import { getLibrary } from '../config/web3'
 import { Web3ReactProvider } from '@web3-react/core'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
 
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Web3ReactProvider>
   )
 }
